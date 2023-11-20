08d0613f105d2a012fd5a4edba01eeccdf49da52
Paulo Sergio <paulosxb@gmail.com>
2023-11-20 09:47:37 -0300

    atualizando javascript
    Atualizando dados

--- Aula-1/script.js
+++ Aula-1/script.js
@@ -1,6 +1,6 @@
-var valorEmDolar = 64;
+var valorEmDolar = 1;
 var cotacaoDoDolar = 5.32;
 
 var valorEmReal = valorEmDolar * cotacaoDoDolar;
 valorEmReal = valorEmReal.toFixed(2);
-alert("R$ "  + valorEmReal);
+alert("Valor do Dolar atualmente - R$ "  + valorEmReal);
