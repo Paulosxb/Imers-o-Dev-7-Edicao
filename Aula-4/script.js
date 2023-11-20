5628d9be3649b83a9d0b5710a8ab2768748c1dd1
Paulo Sergio <paulosxb@gmail.com>
2023-11-20 09:58:19 -0300

    ajustes
    Ajustes gerais nos arquivos das aulas

---
+++ Aula-4/script.js
@@ -0,0 +1,6 @@
+var valorEmDolar = 1;
+var cotacaoDoDolar = 5.32;
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+alert("Valor do Dolar atualmente - R$ "  + valorEmReal);
